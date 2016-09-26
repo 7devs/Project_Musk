f31a4bda27aeb637a856751fc7405465a189edde
DigitalCraftGuy <digitalcraftguy@gmail.com>
2016-09-26 20:58:57 +0800

    菜单

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-var app = require('express')();
-var bodyParser = require('body-parser');
+var app = require('express')(),
+    bodyParser = require('body-parser'),
+    wechat = require('./lib/wechat'),
+    config = require('./lib/config');
 
 app.use(bodyParser.urlencoded({
   extended: false,
 }));
+
+wechat(conf.wechat);
+
+wechat.createMenu(require('./lib/menu.json'));
 
 app.use('/wxapi', require('./lib/routers/wxapi.js'));
 
