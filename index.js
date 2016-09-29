d545aeb3f220b3657c8f785a859eb8db333f6e9e
DigitalCraftGuy <digitalcraftguy@gmail.com>
2016-09-29 10:39:07 +0800

    带注释

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-var app = require('express')();
-var bodyParser = require('body-parser');
-var wechat = require('./lib/wechat');
-var config = require('./lib/config');
+    // 引入配置文件
+var app = require('express')(),
+    bodyParser = require('body-parser'),
+
+    // 引入微信 API 功能包
+    conf = require('./lib/config'),
+    wechat = require('./lib/wechat');
+
 
 app.use(bodyParser.urlencoded({
-  extended: false,
+    extended: false
 }));
 
-wechat(config.wechat);
+// 初始化微信 API 功能包
+wechat(conf.wechat);
 
+// 创建自定义菜单，lib/menu.json 为菜单结构描述文件
+// 相关文档： https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141013&token=&lang=zh_CN
 wechat.createMenu(require('./lib/menu.json'));
 
 app.use('/wxapi', require('./lib/routers/wxapi.js'));
 
-app.get('/', function(req, res, next) {
-  res.send('1474894044034');
-});
-
-app.listen(8015, function(error) {
-  console.log('listenning at 8015...');
-});
+app.listen(8015, function(err) {
+    console.log('listenning at 8015...');
+})
